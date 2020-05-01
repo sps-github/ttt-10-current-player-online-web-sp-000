8b56bf28ecce422d3b9f8ca25559c07d26181327
Sarah Peters <sarahpieters15@gmail.com>
2020-05-01 14:32:34 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -3,3 +3,13 @@ turn_count = ["board 1", "board 2", "board 3", "board 4", "board 5", "board 6", 
 turn_count.each do |board|
   puts "You've had #{board} until now"
 end
+
+def turn_count(board)
+  counter = 0
+  board.each {|space|
+    if space == "X" || space == "O"
+      counter += 1
+    end
+  }
+  counter
+end
