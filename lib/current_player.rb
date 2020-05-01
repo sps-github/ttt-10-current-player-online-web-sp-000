3363ec46354333114d6e3a689a14d2fa8270a893
Sarah Peters <sarahpieters15@gmail.com>
2020-05-01 14:27:45 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -10,6 +10,8 @@ def turn_count(board)
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
 
-turn_count(board1).each do |turn|
+turn_count = ["#{board[0]}", "#{board[1]}", "#{board[2]}", "#{board[3]}", "#{board[4]}", "#{board[5]}", "#{board[6]}", "#{board[7]}", "#{board[8]}"]
+
+turn_count.each do |turn|
   puts "You've had #{turn} until now"
 end
