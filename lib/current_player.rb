f4db7b88147f9fff20abafd8be2c5666e1293db5
Sarah Peters <sarahpieters15@gmail.com>
2020-05-01 14:33:07 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,9 +1,3 @@
-turn_count = ["board 1", "board 2", "board 3", "board 4", "board 5", "board 6", "board 7", "board 8", "board 9"]
-
-turn_count.each do |board|
-  puts "You've had #{board} until now"
-end
-
 def turn_count(board)
   counter = 0
   board.each {|space|
